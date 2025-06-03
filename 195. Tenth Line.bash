420bbf38e2a283e6ada287878333852ccdb4db1c
DogoPro <97559453+dogopro1@users.noreply.github.com>
2025-06-03 14:59:39 +0200

    Update 195. Tenth Line.bash

--- 195. Tenth Line.bash
+++ 195. Tenth Line.bash
@@ -2,7 +2,7 @@ echo -e "Line 1\nLine 2\nLine 3\nLine 4\nLine 5\nLine 6\nLine 7\nLine 8\nLine 9\
 
 
 
-#!/bin/bash
+
 lines=$(wc -l < file.txt)
 if [ "$lines" -lt 10 ]; then
   echo "File has less than 10 lines"
