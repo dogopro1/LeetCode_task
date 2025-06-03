8248093157ba025d7b2db24c5d9c7729bde7cdfb
DogoPro <97559453+dogopro1@users.noreply.github.com>
2025-06-03 14:46:38 +0200

    Create 1122. Relative Sort Array.py

---
+++ 1122. Relative Sort Array.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        order = {num: i for i, num in enumerate(arr2)}
+        
+        return sorted(arr1, key=lambda x: (order.get(x, float('inf')), x))
