090761b1afdb1100753058c190287367953ec711
DogoPro <97559453+dogopro1@users.noreply.github.com>
2025-06-03 14:58:04 +0200

    Create 195. Tenth Line.bash

---
+++ 195. Tenth Line.bash
@@ -0,0 +1,12 @@
+echo -e "Line 1\nLine 2\nLine 3\nLine 4\nLine 5\nLine 6\nLine 7\nLine 8\nLine 9\nLine 10" > file.txt
+
+
+
+#!/bin/bash
+lines=$(wc -l < file.txt)
+if [ "$lines" -lt 10 ]; then
+  echo "File has less than 10 lines"
+else
+  sed -n '10p' file.txt
+fi
+
